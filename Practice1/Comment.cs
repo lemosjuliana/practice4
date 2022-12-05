76af8adee513ca242a1388ad4dfcb3266daa288b
lemosjuliana <lemos.julianajesus@gmail.com>
2022-12-05 20:57:59 -0300

    Practice First Commit

---
+++ Practice1/Comment.cs
@@ -0,0 +1,33 @@
+using System;
+
+
+namespace Practice1
+{
+    /// <summary>
+    /// A small cube with a different number of spots on each of its six sides.
+    /// 
+    /// The responsibility of Die is to keep track of the title, author, and length (in seconds) of the video. 
+    /// </summary> 
+    public class Comment
+    {
+        private string _nameComment;
+        private string _textComment;
+
+        /// <summary>
+        /// Constructs a new instance of Commment
+        /// </summary>
+        public Comment()
+        {
+            
+        }
+           public string GetNameComment()
+        {
+            //
+        }
+          public string GetTextComment()
+        {
+            //
+        }
+
+    }
+}
---
+++ Practice1/Video.cs
@@ -0,0 +1,60 @@
+using System;
+
+
+namespace Practice1
+{
+    /// <summary>
+    /// A small cube with a different number of spots on each of its six sides.
+    /// 
+    /// The responsibility of Die is to keep track of the title, author, and length (in seconds) of the video. 
+    /// </summary> 
+    public class Video
+    {
+        public string _title;
+        public string _author;
+        public int _length = 0;
+        public List<string> _comments;
+        
+
+        /// <summary>
+        /// Constructs a new instance of Die.
+        /// </summary>
+        public Video()
+        {
+            Video video = new Video();
+        }
+
+        /// <summary>
+        /// Generates a new random value and calculates the points for the die.
+        /// </summary>
+        public void CreateVideo()
+        {
+           //
+        }
+         public string SetValue()
+        {
+            //
+        }
+
+           public string AddCommment()
+        {
+            //
+        }
+
+           public string NumberComment()
+        {
+            //
+        }
+           public string DisplayTitle()
+        {
+            //
+        }
+            public string DisplayAuthor()
+        {
+            //
+        }
+
+        
+
+    }
+}
